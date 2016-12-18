37606ff72d2d7839e42f0654934bf01075d36b65
Amiya Sahu <amiya053@gmail.com>
2016-12-18 23:34:49 +0530

    code formatted

--- qa-plugin.php
+++ qa-plugin.php
@@ -1,21 +1,21 @@
 <?php
 /*
-	Plugin Name: Q2A Trumbowyg Editor
-	Plugin URI:
-	Plugin Description: Question2Answer Wrapper for Trumbowyg - A lightweight WYSIWYG editor
-	Plugin Version: 1.0-beta
-	Plugin Date: 2016-12-18
-	Plugin Author: Amiya Sahu
-	Plugin Author URI: http://www.amiyasahu.com
-	Plugin License: MIT
-	Plugin Minimum Question2Answer Version: 1.3
-	Plugin Update Check URI:
+    Plugin Name: Q2A Trumbowyg Editor
+    Plugin URI:
+    Plugin Description: Question2Answer Wrapper for Trumbowyg - A lightweight WYSIWYG editor
+    Plugin Version: 1.0-beta
+    Plugin Date: 2016-12-18
+    Plugin Author: Amiya Sahu
+    Plugin Author URI: http://www.amiyasahu.com
+    Plugin License: MIT
+    Plugin Minimum Question2Answer Version: 1.3
+    Plugin Update Check URI:
 */
 
 
 if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
-	header('Location: ../../');
-	exit;
+    header('Location: ../../');
+    exit;
 }
 
 /**
--- qa-trumbowyg-upload.php
+++ qa-trumbowyg-upload.php
@@ -6,57 +6,57 @@
 class qa_trumbowyg_upload
 {
 
-	/**
-	 * Returns the URL for serving the request 
-	 * @param  string $request 
-	 * @return true | false 
-	 */
-	public function match_request($request)
-	{
-		return ($request == 'trumbowyg-editor-upload');
-	}
+    /**
+     * Returns the URL for serving the request 
+     * @param  string $request 
+     * @return true | false 
+     */
+    public function match_request($request)
+    {
+        return ($request == 'trumbowyg-editor-upload');
+    }
 
-	/**
-	 * process the request for a image upload 
-	 * @param  string $request request for processing 
-	 * @return null
-	 */
-	public function process_request($request)
-	{
-		$message = '';
-		$url = '';
-		$success = false;
+    /**
+     * process the request for a image upload 
+     * @param  string $request request for processing 
+     * @return null
+     */
+    public function process_request($request)
+    {
+        $message = '';
+        $url = '';
+        $success = false;
 
-		if (is_array($_FILES) && count($_FILES)) {
-			
-			if (qa_opt('trumbowyg_editor_upload_images')){
+        if (is_array($_FILES) && count($_FILES)) {
+            
+            if (qa_opt('trumbowyg_editor_upload_images')){
 
-				$upload = qa_upload_file_one(
-					qa_opt('trumbowyg_editor_upload_max_size'),
-					qa_get('qa_only_image'),
-					qa_get('qa_only_image') ? 600 : null, // max width if it's an image upload
-					null // no max height
-				);
+                $upload = qa_upload_file_one(
+                    qa_opt('trumbowyg_editor_upload_max_size'),
+                    qa_get('qa_only_image'),
+                    qa_get('qa_only_image') ? 600 : null, // max width if it's an image upload
+                    null // no max height
+                );
 
-				$message = @$upload['error'];
-				$url = @$upload['bloburl'];
-				$success = empty($upload['error']) ? true : false;	
-				$message = qa_lang('users/no_permission');
-				
-			} else {
-				require_once QA_INCLUDE_DIR.'app/upload.php';
-			}
+                $message = @$upload['error'];
+                $url = @$upload['bloburl'];
+                $success = empty($upload['error']) ? true : false;  
+                $message = qa_lang('users/no_permission');
+                
+            } else {
+                require_once QA_INCLUDE_DIR.'app/upload.php';
+            }
 
-		}
+        }
 
- 		$data = array(
+        $data = array(
             'success' => $success,
             'file'    => qa_js($url),
             'message' => qa_js($message)
         );
- 		
- 		echo json_encode($data);
+        
+        echo json_encode($data);
 
-		return null;
-	}
+        return null;
+    }
 }
